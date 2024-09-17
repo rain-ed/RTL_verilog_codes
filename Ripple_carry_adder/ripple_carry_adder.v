94770b065eb383e33edcd5989e0ffcde92dba47f
rain-ed <rainnlyyy@gmail.com>
2024-09-17 16:10:47 +0530

    4-bit_ripple_carry_adder
    output:-
    a=0001, b=0010, c=0 → sum=0011, carry=0
    a=0101, b=1111, c=0 → sum=0100, carry=1 correctly.
    a=1000, b=0011, c=1 → sum=1100, carry=0
    a=0011, b=0100, c=0 → sum=0111, carry=0

---
+++ Ripple_carry_adder/ripple_carry_adder.v
@@ -0,0 +1,23 @@
+module fa(a,b,c,sum,carry);
+  input a,b,c;
+  output sum,carry;
+  assign sum=a^b^c;
+  assign carry=(a&b)|(b&c)|(a&c);  
+endmodule
+
+
+module pa(a,b,c,sum,carry);
+  input [3:0] a,b;
+  input c;
+  output [3:0]sum;
+  output carry;
+  
+  wire c1,c2,c3;
+         
+  fa f1(a[0],b[0],c,sum[0],c1);
+  fa f2(a[1],b[1],c1,sum[1],c2);
+  fa f3(a[2],b[2],c2,sum[2],c3);
+  fa f4(a[3],b[3],c3,sum[3],carry);
+         
+endmodule
+         
